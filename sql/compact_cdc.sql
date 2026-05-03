588ab8b0e6ce6e377de1a5b9b7d7be7c67d17e64
dsillman2000 <dsillman2000@yahoo.com>
2026-05-03 18:32:01 -0400

    fix: remove explicit sql transaction from compaction script

--- sql/compact_cdc.sql
+++ sql/compact_cdc.sql
@@ -2,9 +2,6 @@
 -- This script reduces the size of the CDC log by keeping only the latest state for each item
 -- and inserting a 'compact' record to signal clients to reset their local storage.
 
--- Wrap in a transaction to ensure atomicity
-BEGIN TRANSACTION;
-
 -- 1. Create a temporary table to store the latest state of all non-deleted items
 -- We exclude 'compaction-signal' to prevent it from being re-inserted as a 'create' record.
 CREATE TEMP TABLE latest_states AS
@@ -34,5 +31,3 @@ FROM latest_states;
 
 -- 5. Cleanup
 DROP TABLE latest_states;
-
-COMMIT;
